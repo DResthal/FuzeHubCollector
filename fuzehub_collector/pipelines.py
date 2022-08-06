06166ee60638f1615246766ae2419713db126e49
NeilClack <c.neilclack@gmail.com>
2022-08-05 21:47:00 -0500

    Created items and item pipeline
    Project succesfully utilizes the printables graphql endpoint to pull raw JSON rather than rendering the javascript site. The model data currently saves in a sqlite database as a proof of concept rather than a working copy. Next is to connect to PostgreSQL and create a working upsert pipeline.

--- fuzehub_collector/pipelines.py
+++ fuzehub_collector/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
 
 class FuzehubCollectorPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect("/var/lib/databases/fuzehubmodels.db")
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS models(
+            id REAL PRIMARY KEY,
+            name TEXT,
+            likes REAL,
+            url TEXT,
+            downloads REAL,
+            last_update TEXT,
+            images TEXT)
+        """
+        )
+
     def process_item(self, item, spider):
+        self.cur.execute(
+            """INSERT OR REPLACE INTO models VALUES (?,?,?,?,?,?,?)""",
+            (
+                item["id"],
+                item["name"],
+                item["likes"],
+                item["downloads"],
+                item["url"],
+                item["last_update"],
+                str(item["images"]),
+            )
+        )
+        self.con.commit()
         return item
