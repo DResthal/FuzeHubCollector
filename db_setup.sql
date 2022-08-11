bd7815f492d81bf108a50e9387b853e7463a511d
NeilClack <c.neilclack@gmail.com>
2022-08-11 17:17:42 -0500

    Removed all old application components

--- db_setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS models (
-	id INTEGER NOT NULL PRIMARY KEY,
-	name VARCHAR(76) NOT NULL,
-	likes INTEGER NOT NULL,
-	url TEXT,
-	downloads INTEGER,
-	last_update TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'),
-	date_added TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'));
